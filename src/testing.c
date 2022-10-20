cd21fdb72974084fac4e0848843cd6d678d7febd
Imad Saad <em4d2002@gmail.com>
2022-10-20 09:56:49 +0400

    GAME

--- src/testing.c
+++ src/testing.c
@@ -54,7 +54,7 @@ int	key(int keycode, t_all *all)
 	}
 	if(keycode == 0)
 	{
-		if(all->map[(int)(all->size->posX + all->size->dirY * all->size->moveSpeed)][(int)(all->size->posY)] != '1') all->size->posX -= all->size->dirY * all->size->moveSpeed;
+		if(all->map[(int)(all->size->posX - all->size->dirY * all->size->moveSpeed)][(int)(all->size->posY)] != '1') all->size->posX -= all->size->dirY * all->size->moveSpeed;
 		if(all->map[(int)(all->size->posX)][(int)(all->size->posY + all->size->dirX * all->size->moveSpeed)] != '1') all->size->posY += all->size->dirX * all->size->moveSpeed;
 		mlx_clear_window(all->mlx->mlx, all->mlx->mlx_win);
 		print_plz(all, all->map);
